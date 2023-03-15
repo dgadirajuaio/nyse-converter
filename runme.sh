b075d49256b690b99dd402b5c7725e13546f498f
dgadirajuaio <durga.gadiraju@analytiqs.io>
2023-03-15 15:33:21 -0500

    Added script to run in non dev environments

---
+++ runme.sh
@@ -0,0 +1,6 @@
+source /Users/itversity/Projects/nyse-converter/nc-venv/bin/activate
+export SRC_DIR=/Users/itversity/Projects/nyse-converter/data/nyse_all/nyse_data
+export LOG_FILE_PATH=/Users/itversity/Projects/nyse-converter/logs/nc.log
+rm -rf /Users/itversity/Projects/nyse-converter/data/nyse_all/nyse_json
+nysec
+deactivate
